bb63dcc038347c0665db82ae004a5bc78b87c36e
CWD <cwd@cloudspokes.com>
2020-01-24 11:17:21 -0500

    finished tests

--- tests/step_impl.py
+++ tests/step_impl.py
@@ -1,19 +1,19 @@
 
 from getgauge.python import Messages
 from getgauge.python import step
+from distutils.util import strtobool
 from submission import solution
 
 
 @step("generate fibonacci number <table>")
 def generate_fibonacci_number(table):
-    assert 1 == solution.calculateFibonacci(1)
-    assert 55 == solution.calculateFibonacci(10)
+    for row in table.rows:
+        assert int(row[1]) == solution.calculateFibonacci(int(row[0]))
 
 
 @step("validate fibonacci number <table>")
 def validate_fibonacci_number(table):
-    print (solution.isFibonacci(9) )
-    assert False == solution.isFibonacci(9)
-    print (solution.isFibonacci(55) )
-    assert True == solution.isFibonacci(55)
+    for row in table.rows:
+        assert strtobool(row[1]) == solution.isFibonacci(int(row[0]))
+
 
